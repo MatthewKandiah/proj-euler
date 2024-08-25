439788204e45a395f32d8fc8a0f1a44a2cc06195
Matthew Kandiah <mj.kandiah@googlemail.com>
2024-08-25 19:49:21 +0100

    Complete ex1 and ex2

--- src/ex1.zig
+++ src/ex1.zig
@@ -1,5 +1,11 @@
 const std = @import("std");
 
 pub fn main() void {
-    std.debug.print("1\n", .{});
+    var sum: usize = 0;
+    for (1..1000) |i| {
+        if (i % 3 == 0 or i % 5 == 0) {
+            sum += i;
+        }
+    }
+    std.debug.print("{}\n", .{sum});
 }
--- src/ex2.zig
+++ src/ex2.zig
@@ -1,5 +1,16 @@
 const std = @import("std");
 
 pub fn main() void {
-    std.debug.print("2\n", .{});
+    var fib1: usize = 1;
+    var fib2: usize = 2;
+    var sum: usize = 0;
+    while (fib2 < 4_000_000) {
+        if (fib2 % 2 == 0) {
+            sum += fib2;
+        }
+        const temp: usize = fib1 + fib2;
+        fib1 = fib2;
+        fib2 = temp;
+    }
+    std.debug.print("{}\n", .{sum});
 }
