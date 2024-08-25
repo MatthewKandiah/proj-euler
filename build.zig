76421be8ba08774d43bf855001aa77708b6c0f8b
Matthew Kandiah <mj.kandiah@googlemail.com>
2024-08-25 14:23:52 +0100

    tweak build.zig to allow running a single specified exercise

--- build.zig
+++ build.zig
@@ -1,14 +1,19 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
+    const ex_num = b.option(usize, "ex", "Exercise number") orelse @panic("You have to provide an exercise number");
+    var buf1: [32]u8 = undefined;
+    var buf2: [32]u8 = undefined;
+    const ex_name = try std.fmt.bufPrint(&buf1, "ex{}", .{ex_num});
+    const ex_path = try std.fmt.bufPrint(&buf2, "src/ex{}.zig", .{ex_num});
     const exe = b.addExecutable(.{
-        .name = "hello",
-        .root_source_file = b.path("hello.zig"),
+        .name = ex_name,
+        .root_source_file = b.path(ex_path),
         .target = b.host,
     });
     b.installArtifact(exe);
 
     const run_exe = b.addRunArtifact(exe);
-    const run_step = b.step("run", "Run the application");
+    const run_step = b.step("run", "Run the exercise");
     run_step.dependOn(&run_exe.step);
 }
