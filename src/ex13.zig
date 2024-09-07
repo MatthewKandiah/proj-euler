83c1ee2a9d727e071a5934f27f660cf3e6d32640
Matthew Kandiah <mj.kandiah@googlemail.com>
2024-09-07 04:52:55 +0100

    went back and did ex13, it wasn't that boring

--- src/ex13.zig
+++ src/ex13.zig
@@ -4,13 +4,18 @@ pub fn main() !void {
     var input_file = try std.fs.cwd().openFile("src/ex13_input.txt", .{});
     defer input_file.close();
 
-    var buf_reader = std.io.bufferedReader(input_file.reader());
-    var in_stream = buf_reader.reader();
+    var in_stream = input_file.reader();
 
-    var read_buf: [5000]u8 = undefined;
+    // reads 50 digits and a delimiter
+    var read_buf: [51]u8 = undefined;
+    var sum: usize = 0;
     while (try in_stream.readUntilDelimiterOrEof(&read_buf, '\n')) |line| {
-        std.debug.print("{s}\n", .{line});
+        // if you add 50 one-digit numbers, the max possible result is 450
+        // so to get the correct first 10 digits of the sum, we only need to add the first 12 digits of each number, the less significant figures are inconsequential noise
+        const leading_digits = try std.fmt.parseInt(usize, line[0..12], 10);
+        sum += leading_digits;
     }
-
-    @panic("Seems like a boring problem, skipping for now");
+    var result_buf: [14]u8 = .{'0'} ** 14;
+    const result = try std.fmt.bufPrint(&result_buf, "{}", .{sum});
+    std.debug.print("{s}\n", .{result[0..10]});
 }
