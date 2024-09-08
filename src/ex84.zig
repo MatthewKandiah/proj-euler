7606c795dc98185b747512136a2df63ed3151161
Matthew Kandiah <mj.kandiah@googlemail.com>
2024-09-08 17:34:56 +0100

    wip - getting the wrong answers, think it might be because I'm not simulating the decks of cards properly

--- src/ex84.zig
+++ src/ex84.zig
@@ -1,7 +1,8 @@
 const std = @import("std");
 
-const num_spaces = 39;
+const num_spaces = 40;
 const dice_sides = 6;
+const convergence_tolerance = 0.000_001;
 
 const BoardSpace = enum(usize) {
     go = 0,
@@ -44,22 +45,42 @@ const BoardSpace = enum(usize) {
     h1,
     t2,
     h2,
+
+    const Self = @This();
+
+    fn nextRail(self: Self) Self {
+        return switch (self) {
+            .r4, .ch3, .h1, .t2, .h2, .go, .a1, .cc1, .a2, .t1 => .r1,
+            .r1, .b1, .ch1, .b2, .b3, .jail, .c1, .u1, .c2, .c3 => .r2,
+            .r2, .d1, .cc2, .d2, .d3, .fp, .e1, .ch2, .e2, .e3 => .r3,
+            .r3, .f1, .f2, .u2, .f3, .g2j, .g1, .g2, .cc3, .g3 => .r4,
+        };
+    }
+
+    fn nextUtility(self: Self) Self {
+        return switch (self) {
+            .u1, .c2, .c3, .r2, .d1, .cc2, .d2, .d3, .fp, .e1, .ch2, .e2, .e3, .r3, .f1, .f2 => .u2,
+            .u2, .f3, .g2j, .g1, .g2, .cc3, .g3, .r4, .ch3, .h1, .t2, .h2, .go, .a1, .cc1, .a2, .t1, .r1, .b1, .ch1, .b2, .b3, .jail, .c1 => .u1,
+        };
+    }
 };
 
 const MonopolyContext = struct {
     num_visits: [num_spaces]u32,
     current_space: BoardSpace,
     doubles_rolled: u32,
+    rng: *std.rand.Xoshiro256,
 
     const Self = @This();
 
-    fn init() Self {
+    fn init(rng: *std.rand.Xoshiro256) Self {
         var num_visits: [num_spaces]u32 = .{0} ** num_spaces;
         num_visits[0] = 1;
         return Self{
             .num_visits = num_visits,
             .current_space = .go,
             .doubles_rolled = 0,
+            .rng = rng,
         };
     }
 
@@ -83,34 +104,81 @@ const MonopolyContext = struct {
     }
 
     fn handleCommunityChest(self: *Self, space: BoardSpace) void {
-        _ = self;
-        _ = space;
+        self.goToSpace(switch (self.rollDice(16)) {
+            1 => .go,
+            2 => .jail,
+            3...16 => space,
+            else => unreachable,
+        });
     }
 
     fn handleChance(self: *Self, space: BoardSpace) void {
-        _ = self;
-        _ = space;
+        self.goToSpace(switch (self.rollDice(16)) {
+            1 => .go,
+            2 => .jail,
+            3 => .c1,
+            4 => .e3,
+            5 => .h2,
+            6 => .r1,
+            7, 8 => space.nextRail(),
+            9 => space.nextUtility(),
+            10 => @enumFromInt(@intFromEnum(space) - 3),
+            11...16 => space,
+            else => unreachable,
+        });
     }
 
     fn goToSpace(self: *Self, space: BoardSpace) void {
         self.current_space = space;
         self.num_visits[@intFromEnum(space)] += 1;
     }
+
+    fn rollDice(self: *Self, sides: u32) u32 {
+        return @intCast(self.rng.next() % sides + 1);
+    }
+
+    fn getSpaceProbabilities(self: Self, count: u32) [num_spaces]f32 {
+        var result: [num_spaces]f32 = undefined;
+        for (0..num_spaces) |i| {
+            result[i] = @as(f32, @floatFromInt(self.num_visits[i])) / @as(f32, @floatFromInt(count));
+        }
+        return result;
+    }
 };
 
 pub fn main() void {
     var rng = std.rand.DefaultPrng.init(42);
-    var context = MonopolyContext.init();
+    var context = MonopolyContext.init(&rng);
 
     var finished = false;
-    while (!finished) {
-        const roll1 = rollDice(&rng, dice_sides);
-        const roll2 = rollDice(&rng, dice_sides);
+    var count: u32 = 0;
+    const runs_between_checks = 1_000_000;
+    var space_probabilities: [num_spaces]f32 = .{0} ** num_spaces;
+    while (!finished) : (count += 1) {
+        const roll1 = context.rollDice(dice_sides);
+        const roll2 = context.rollDice(dice_sides);
         context.move(roll1, roll2);
-        finished = true;
+        if (count > 0 and count % runs_between_checks == 0) {
+            const next_space_probabilities = context.getSpaceProbabilities(count);
+            if (probabilitiesConverged(next_space_probabilities, space_probabilities, convergence_tolerance)) {
+                finished = true;
+            } else {
+                space_probabilities = next_space_probabilities;
+            }
+        }
     }
+    // check known values for 6 sided dice
+    std.debug.print("jail: {}\n", .{space_probabilities[@intFromEnum(BoardSpace.jail)]}); // expected 0.0624
+    std.debug.print("e3: {}\n", .{space_probabilities[@intFromEnum(BoardSpace.e3)]}); // expected 0.0318
+    std.debug.print("jail: {}\n", .{space_probabilities[@intFromEnum(BoardSpace.jail)]}); // expected 0.0309
+    std.debug.print("g2j: {}\n", .{space_probabilities[@intFromEnum(BoardSpace.g2j)]}); // expected 0
 }
 
-fn rollDice(rng: *std.rand.Xoshiro256, sides: u32) u32 {
-    return @intCast(rng.next() % sides);
+pub fn probabilitiesConverged(probs1: [num_spaces]f32, probs2: [num_spaces]f32, tolerance: f32) bool {
+    for (probs1, probs2) |x, y| {
+        if (@abs(x - y) > tolerance) {
+            return false;
+        }
+    }
+    return true;
 }
